31863e1bbc337bb60341356327cbc71ce9dc73d9
userMeatball <cameron-bradley@hotmail.com>
2023-02-23 16:49:39 +0000

    dogs migration blank welcome

---
+++ database/migrations/2023_02_23_154615_create_dogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dogs', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->tinyInteger('age');
+            $table->text('sex');
+            $table->text('breed');
+            $table->text('colour');
+            $table->text('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dogs');
+    }
+};
